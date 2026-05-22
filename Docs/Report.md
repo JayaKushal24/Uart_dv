ab70624bec1690e2b397416809de278a08ec05de
JayaKushal24 <jayakushal2004@gmail.com>
2026-05-22 09:55:53 +0530

    .

---
+++ Docs/Report.md
@@ -0,0 +1,5 @@
+# Report Link
+
+## link
+
+https://drive.google.com/drive/folders/1LcamC1fiWPiOJZ4yfJbDXIQpQnE_y7TW
--- SRC/Design/u_rec.v
+++ SRC/Design/u_rec.v
@@ -1,33 +1,3 @@
-module baud#(parameter BAUD =115200,CLK_FREQ=100_000_000)(
-input sys_clk,sys_rst_l,
-output reg uart_clk
-    );
-
-localparam CLK_DIV=(CLK_FREQ)/(BAUD*16);//(100_000_000/115200)=(868/16)=54..
-//localparam CLK_DIV=(CLK_FREQ)/(BAUD*16*2);
-reg [$clog2(CLK_DIV/2)-1:0]count;
-
-    always @(posedge sys_clk or negedge sys_rst_l)begin
-        if(!sys_rst_l)begin
-            uart_clk<=0;
-            count<=0;
-        end
-        else begin
-                if(count==CLK_DIV/2-1)begin
-                    uart_clk<=~uart_clk;
-                    count<=0;
-                end
-                else count<=count+1;
-        end
-    end
-
-
-endmodule
-
-[jayakushal@feserver my_uart]$
-[jayakushal@feserver my_uart]$ cat u_rec.v
-`timescale 1ns / 1ps
-
 module u_rec #(parameter DATA_WIDTH=8) (
     input sys_rst_l, uart_clk, uart_REC_dataH,
     output reg rec_readyH, rec_busyH,
